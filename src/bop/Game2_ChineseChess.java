6b7456b4e2b3b1c1f744e5afadba7f575e907c15
If <ifhuang91@gmail.com>
2014-11-08 18:45:31 +0800

    chinese chess, pancake

---
+++ src/bop/Game2_ChineseChess.java
@@ -0,0 +1,17 @@
+package bop;
+
+public class Game2_ChineseChess
+{
+	public void printKings()
+	{
+		for (int i = 1; i <= 81; i++)
+			if (i / 9 % 3 != i % 9 % 3)
+				System.out.printf("A = %d, B = %d\n", i / 9 + 1, i % 9 + 1);
+	}
+
+	public static void main(String[] args)
+	{
+		Game2_ChineseChess game2 = new Game2_ChineseChess();
+		game2.printKings();
+	}
+}
