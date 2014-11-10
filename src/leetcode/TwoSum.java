7f49f3b69c8b09309d58ce830efa6cfacba8780a
If <ifhuang91@gmail.com>
2014-11-11 02:00:38 +0800

    update

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,30 +1,21 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
+// https://oj.leetcode.com/problems/two-sum/
 public class TwoSum
 {
 	public int[] twoSum(int[] numbers, int target)
 	{
-		int[] result = new int[2];
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < numbers.length; i++)
 		{
-			int remain = target - numbers[i];
-			if (map.containsKey(remain))
-			{
-				result[0] = map.get(remain);
-				result[1] = i + 1;
-				break;
-			}
-			else
-			{
-				if (!map.containsKey(numbers[i]))
-				{
-					map.put(numbers[i], i + 1);
-				}
-			}
+			int r = target - numbers[i];
+			if (map.containsKey(r))
+				return new int[] { map.get(r) + 1, i + 1 };
+			map.put(numbers[i], i);
 		}
-		return result;
+		throw new IllegalArgumentException("no solution");
 	}
 }
