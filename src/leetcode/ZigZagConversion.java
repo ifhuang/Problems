f2ef9b30fec9c6071b067b7e8eff122d68ad485c
If <ifhuang91@gmail.com>
2014-11-15 03:22:15 +0800

    update

--- src/leetcode/ZigZagConversion.java
+++ src/leetcode/ZigZagConversion.java
@@ -1,53 +1,40 @@
 package leetcode;
 
+// https://oj.leetcode.com/problems/zigzag-conversion/
 public class ZigZagConversion
 {
 	public String convert(String s, int nRows)
 	{
-		if (s == null || nRows < 1)
-			return null;
-		else if (nRows == 1)
-		{
+		int n = s.length();
+		if (nRows == 1 || n <= nRows)
 			return s;
-		}
-		else
+		StringBuilder[] sb = new StringBuilder[nRows];
+		for (int i = 0; i < nRows; i++)
+			sb[i] = new StringBuilder();
+		for (int i = 0; i < n; i += 2 * nRows - 2)
+			sb[0].append(s.charAt(i));
+		for (int i = nRows - 1; i < n; i += 2 * nRows - 2)
+			sb[nRows - 1].append(s.charAt(i));
+		for (int i = 1; i < nRows - 1; i++)
 		{
-			StringBuffer[] sb = new StringBuffer[nRows];
-			for (int i = 0; i < sb.length; i++)
+			int flag = 0;
+			for (int j = i; j < n;)
 			{
-				sb[i] = new StringBuffer();
-			}
-			for (int i = 0; i < s.length(); i += 2 * nRows - 2)
-			{
-				sb[0].append(s.charAt(i));
-			}
-			for (int i = nRows - 1; i < s.length(); i += 2 * nRows - 2)
-			{
-				sb[sb.length - 1].append(s.charAt(i));
-			}
-			for (int i = 1; i < nRows - 1; i++)
-			{
-				int flag = 0;
-				for (int j = i; j < s.length();)
+				sb[i].append(s.charAt(j));
+				if (flag == 0)
 				{
-					sb[i].append(s.charAt(j));
-					if (flag == 0)
-					{
-						j += 2 * nRows - 2 - i * 2;
-						flag = 1;
-					}
-					else
-					{
-						j += i * 2;
-						flag = 0;
-					}
+					j += 2 * nRows - 2 - 2 * i;
+					flag = 1;
+				}
+				else
+				{
+					j += 2 * i;
+					flag = 0;
 				}
 			}
-			for (int i = 1; i < sb.length; i++)
-			{
-				sb[0].append(sb[i]);
-			}
-			return sb[0].toString();
 		}
+		for (int i = 1; i < nRows; i++)
+			sb[0].append(sb[i]);
+		return sb[0].toString();
 	}
 }
