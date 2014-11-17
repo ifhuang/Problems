c83d32e52c60c4af265c80dec3c2dc8f6e05800c
If <ifhuang91@gmail.com>
2014-11-17 22:09:05 +0800

    binary, factorial, find

---
+++ src/bop/Number2_Factorial.java
@@ -0,0 +1,43 @@
+package bop;
+
+public class Number2_Factorial
+{
+	public int countZero(int n)
+	{
+		int count = 0;
+		while (n != 0)
+		{
+			count += n / 5;
+			n /= 5;
+		}
+		return count;
+	}
+
+	public int firstOne(int n)
+	{
+		int count = 0;
+		while (n != 0)
+		{
+			count += n / 2;
+			n /= 2;
+		}
+		return count + 1;
+	}
+
+	private int f(int n)
+	{
+		int ans = 1;
+		for (int i = 2; i <= n; i++)
+			ans *= i;
+		return ans;
+	}
+
+	public static void main(String[] args)
+	{
+		Number2_Factorial n2 = new Number2_Factorial();
+		int num = 10;
+		System.out.println(n2.countZero(num));
+		System.out.println(Integer.toBinaryString(n2.f(num)));
+		System.out.println(n2.firstOne(num));
+	}
+}
---
+++ src/bop/Number3_Find.java
@@ -0,0 +1,55 @@
+package bop;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Number3_Find
+{
+	public int find(int[] id)
+	{
+		int now = id[0];
+		int count = 1;
+		for (int i = 1; i < id.length; i++)
+			if (count == 0)
+			{
+				now = id[i];
+				count++;
+			}
+			else if (now != id[i])
+				count--;
+			else
+				count++;
+		return now;
+	}
+
+	public Integer[] findN(int[] id, int n)
+	{
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < id.length; i++)
+		{
+			if (map.containsKey(id[i]))
+				map.put(id[i], map.get(id[i]) + 1);
+			else if (map.size() < n - 1)
+				map.put(id[i], 1);
+			else
+			{
+				for (int key : map.keySet())
+					if (map.get(key) > 1)
+						map.put(key, map.get(key) - 1);
+					else
+						map.remove(key);
+			}
+		}
+		return map.keySet().toArray(new Integer[0]);
+	}
+
+	public static void main(String[] args)
+	{
+		Number3_Find n3 = new Number3_Find();
+		int[] id = new int[] { 1, 1, 2, 2, 2, 2, 5, 5, 5, 1, 1, 1, 1, 1, 1 };
+		System.out.println(n3.find(id));
+		int[] idN = new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 5 };
+		System.out.println(Arrays.toString(n3.findN(idN, 3)));
+	}
+}
