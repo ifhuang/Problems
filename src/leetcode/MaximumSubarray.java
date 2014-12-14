9a47b493cff5ab38ac737f5529ca8ccff2a36c00
Yifu <ifhuang91@gmail.com>
2014-12-14 23:47:40 +0800

    update

--- src/leetcode/MaximumSubarray.java
+++ src/leetcode/MaximumSubarray.java
@@ -1,49 +1,17 @@
 package leetcode;
 
-import java.util.Arrays;
-
-public class MaximumSubarray
-{
-	public int maxSubArray(int[] A)
-	{
-		int high = A.length;
-		if (high == 1)
-		{
-			return A[0];
+// https://oj.leetcode.com/problems/maximum-subarray/
+public class MaximumSubarray {
+	public int maxSubArray(int[] A) {
+		int len = A.length;
+		int maxSum = A[0];
+		int tmpSum = A[0];
+		for (int i = 1; i < len; i++) {
+			if (tmpSum < 0)
+				tmpSum = 0;
+			tmpSum += A[i];
+			maxSum = Math.max(maxSum, tmpSum);
 		}
-		else if (high == 2)
-		{
-			return Math.max(Math.max(A[0], A[1]), A[0] + A[1]);
-		}
-		int mid = high / 2;
-
-		int maxLeft = maxSubArray(Arrays.copyOfRange(A, 0, mid));
-
-		int maxRight = maxSubArray(Arrays.copyOfRange(A, mid + 1, high));
-
-		int maxSumLeft = Integer.MIN_VALUE;
-		int sumLeft = 0;
-		for (int i = mid; i >= 0; i--)
-		{
-			sumLeft += A[i];
-			if (sumLeft > maxSumLeft)
-				maxSumLeft = sumLeft;
-		}
-		int maxSumRight = Integer.MIN_VALUE;
-		int sumRight = 0;
-		for (int i = mid; i < high; i++)
-		{
-			sumRight += A[i];
-			if (sumRight > maxSumRight)
-				maxSumRight = sumRight;
-		}
-		int maxMid = maxSumLeft + maxSumRight - A[mid];
-		return Math.max(Math.max(maxLeft, maxRight), maxMid);
-	}
-
-	public static void main(String[] args)
-	{
-		MaximumSubarray s = new MaximumSubarray();
-		System.out.println(s.maxSubArray(new int[] { -1, 0, -2 }));
+		return maxSum;
 	}
 }
--- src/leetcode/SpiralMatrix.java
+++ src/leetcode/SpiralMatrix.java
@@ -1,107 +1,34 @@
 package leetcode;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SpiralMatrix
-{
-	public List<Integer> spiralOrder(int[][] matrix)
-	{
-		List<Integer> list = new LinkedList<>();
-		if (matrix == null || matrix.length == 0)
-			return list;
-		else
-		{
-			int x = 0;
-			int y = 0;
-			int m = matrix.length;
-			int n = matrix[0].length;
-			list.add(matrix[x][y]);
-			matrix[x][y] = Integer.MAX_VALUE;
-			int state = 0;
-			int count = 2;
-			int max = m * n;
-			while (count <= max)
-			{
-				if (state == 0)
-				{
-					int x1 = x;
-					int y1 = y + 1;
-					if (check(matrix, x1, y1))
-					{
-						y++;
-						list.add(matrix[x][y]);
-						matrix[x][y] = Integer.MAX_VALUE;
-						count++;
-					}
-					else
-						state = (state + 1) % 4;
-				}
-				else if (state == 1)
-				{
-					int x1 = x + 1;
-					int y1 = y;
-					if (check(matrix, x1, y1))
-					{
-						x++;
-						list.add(matrix[x][y]);
-						matrix[x][y] = Integer.MAX_VALUE;
-						count++;
-					}
-					else
-						state = (state + 1) % 4;
-				}
-				else if (state == 2)
-				{
-					int x1 = x;
-					int y1 = y - 1;
-					if (check(matrix, x1, y1))
-					{
-						y--;
-						list.add(matrix[x][y]);
-						matrix[x][y] = Integer.MAX_VALUE;
-						count++;
-					}
-					else
-						state = (state + 1) % 4;
-				}
-				else if (state == 3)
-				{
-					int x1 = x - 1;
-					int y1 = y;
-					if (check(matrix, x1, y1))
-					{
-						x--;
-						list.add(matrix[x][y]);
-						matrix[x][y] = Integer.MAX_VALUE;
-						count++;
-					}
-					else
-						state = (state + 1) % 4;
-				}
-			}
-			return list;
+// https://oj.leetcode.com/problems/spiral-matrix/
+public class SpiralMatrix {
+	public List<Integer> spiralOrder(int[][] matrix) {
+		List<Integer> ans = new ArrayList<>();
+		if (matrix.length == 0)
+			return ans;
+		int rB = 0;
+		int rE = matrix.length - 1;
+		int cB = 0;
+		int cE = matrix[0].length - 1;
+		while (rB <= rE && cB <= cE) {
+			for (int i = cB; i <= cE; i++)
+				ans.add(matrix[rB][i]);
+			rB++;
+			for (int i = rB; i <= rE; i++)
+				ans.add(matrix[i][cE]);
+			cE--;
+			if (rB <= rE)
+				for (int i = cE; i >= cB; i--)
+					ans.add(matrix[rE][i]);
+			rE--;
+			if (cB <= cE)
+				for (int i = rE; i >= rB; i--)
+					ans.add(matrix[i][cB]);
+			cB++;
 		}
-	}
-
-	private boolean check(int[][] result, int x, int y)
-	{
-		int m = result.length;
-		int n = result[0].length;
-		if (x >= m || y >= n || x < 0 || y < 0)
-			return false;
-		else if (result[x][y] == Integer.MAX_VALUE)
-			return false;
-		else
-			return true;
-	}
-
-	public static void main(String[] args)
-	{
-		SpiralMatrix solution = new SpiralMatrix();
-		int[][] a = new int[1][2];
-		a[0][0] = 1;
-		a[0][1] = 2;
-		System.out.println(solution.spiralOrder(a));
+		return ans;
 	}
 }
