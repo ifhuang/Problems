c4407df0fe8e2af4b6a49aa2049d9e428f2341d6
Yifu Huang <ifhuang91@gmail.com>
2015-01-29 01:42:09 +0800

    update

--- src/leetcode/PascalsTriangle.java
+++ src/leetcode/PascalsTriangle.java
@@ -1,39 +1,27 @@
 package leetcode;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PascalsTriangle
-{
-	public List<List<Integer>> generate(int numRows)
-	{
-		List<List<Integer>> result = new LinkedList<>();
-		if (numRows == 0)
-			return result;
-		List<Integer> item = new LinkedList<>();
-		item.add(1);
-		result.add(item);
-		if (numRows == 1)
-			return result;
-		item = new LinkedList<>();
-		item.add(1);
-		item.add(1);
-		result.add(item);
-		if (numRows == 2)
-			return result;
-		List<Integer> pre = item;
-		for (int i = 0; i < numRows - 2; i++)
-		{
-			item = new LinkedList<>();
-			item.add(1);
-			for (int j = 0; j <= i; j++)
-			{
-				item.add(pre.get(j) + pre.get(j + 1));
-			}
-			item.add(1);
-			result.add(item);
-			pre = item;
+// https://oj.leetcode.com/problems/pascals-triangle/
+public class PascalsTriangle {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> ans = new ArrayList<>();
+		if (numRows == 1) {
+			List<Integer> list = new ArrayList<>();
+			list.add(1);
+			ans.add(list);
+		} else if (numRows > 1) {
+			List<List<Integer>> pre = generate(numRows - 1);
+			ans.addAll(pre);
+			List<Integer> last = pre.get(numRows - 2);
+			List<Integer> list = new ArrayList<>();
+			list.add(1);
+			for (int i = 0; i < numRows - 2; i++)
+				list.add(last.get(i) + last.get(i + 1));
+			list.add(1);
+			ans.add(list);
 		}
-		return result;
+		return ans;
 	}
 }
--- src/leetcode/PascalsTriangleII.java
+++ src/leetcode/PascalsTriangleII.java
@@ -1,31 +1,21 @@
 package leetcode;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PascalsTriangleII
-{
-	public List<Integer> getRow(int rowIndex)
-	{
-		List<Integer> result = new LinkedList<>();
-		result.add(1);
-		if (rowIndex == 0)
-			return result;
-		result.add(1);
-		if (rowIndex == 1)
-			return result;
-		List<Integer> pre = result;
-		for (int i = 0; i < rowIndex - 1; i++)
-		{
-			result = new LinkedList<>();
-			result.add(1);
-			for (int j = 0; j <= i; j++)
-			{
-				result.add(pre.get(j) + pre.get(j + 1));
-			}
-			result.add(1);
-			pre = result;
+// https://oj.leetcode.com/problems/pascals-triangle-ii/
+public class PascalsTriangleII {
+	public List<Integer> getRow(int rowIndex) {
+		List<Integer> ans = new ArrayList<>();
+		if (rowIndex == 0) {
+			ans.add(1);
+		} else {
+			List<Integer> pre = getRow(rowIndex - 1);
+			ans.add(1);
+			for (int i = 0; i < rowIndex - 1; i++)
+				ans.add(pre.get(i) + pre.get(i + 1));
+			ans.add(1);
 		}
-		return result;
+		return ans;
 	}
 }
