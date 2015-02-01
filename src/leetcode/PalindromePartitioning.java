fdf5a1f80e44340a3434d29e04d6051118dba97e
Yifu Huang <ifhuang91@gmail.com>
2015-02-01 17:16:47 +0800

    update

--- src/leetcode/PalindromePartitioning.java
+++ src/leetcode/PalindromePartitioning.java
@@ -1,57 +1,39 @@
 package leetcode;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PalindromePartitioning
-{
-	public List<List<String>> partition(String s)
-	{
-		List<List<String>> list = new LinkedList<>();
-		if (s == null || s.length() == 0)
-			return list;
-		else if (s.length() == 1)
-		{
-			List<String> item = new LinkedList<>();
-			item.add(s);
-			list.add(item);
-			return list;
-		}
-		else
-		{
-			int length = s.length();
-			for (int i = 1; i <= length; i++)
-			{
-				String pre = s.substring(0, i);
-				String suf = s.substring(i);
-				if (check(pre))
-				{
-					List<List<String>> sufList = partition(suf);
-					for (List<String> list2 : sufList)
-					{
-						list2.add(0, pre);
-						list.add(list2);
-					}
-					if (suf.length() == 0)
-					{
-						List<String> item = new LinkedList<>();
-						item.add(pre);
-						list.add(item);
-					}
-				}
-			}
-			return list;
-		}
-	}
+// https://oj.leetcode.com/problems/palindrome-partitioning/
+public class PalindromePartitioning {
+  public List<List<String>> partition(String s) {
+    int n = s.length();
+    List<List<String>> ans = new ArrayList<>();
+    if (check(s)) {
+      List<String> list = new ArrayList<>();
+      list.add(s);
+      ans.add(list);
+    }
+    if (n > 1)
+      for (int i = 1; i < n; i++) {
+        String pre = s.substring(0, i);
+        String post = s.substring(i);
+        if (check(post)) {
+          List<List<String>> preAns = partition(pre);
+          for (List<String> preList : preAns) {
+            List<String> list = new ArrayList<>(preList);
+            list.add(post);
+            ans.add(list);
+          }
+        }
+      }
+    return ans;
+  }
 
-	private boolean check(String s)
-	{
-		if (s.length() == 1)
-			return true;
-		int length = s.length();
-		for (int i = 0; i < length / 2; i++)
-			if (s.charAt(i) != s.charAt(length - 1 - i))
-				return false;
-		return true;
-	}
+  private boolean check(String s) {
+    int n = s.length();
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+      if (s.charAt(i) != s.charAt(j))
+        return false;
+    return true;
+  }
 }
--- src/leetcode/SurroundedRegions.java
+++ src/leetcode/SurroundedRegions.java
@@ -2,86 +2,61 @@ package leetcode;
 
 import java.util.Stack;
 
-public class SurroundedRegions
-{
-	class Pair
-	{
-		int x;
-		int y;
+// https://oj.leetcode.com/problems/surrounded-regions/
+public class SurroundedRegions {
+  class Pair {
+    int x;
+    int y;
 
-		public Pair(int x1, int y1)
-		{
-			x = x1;
-			y = y1;
-		}
-	}
+    Pair(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
 
-	public void solve(char[][] board)
-	{
-		if (board == null)
-			return;
-		int x = board.length;
-		if (x < 2)
-			return;
-		int y = board[0].length;
-		if (y < 2)
-			return;
+  public void solve(char[][] board) {
+    int m = board.length;
+    if (m < 2)
+      return;
+    int n = board[0].length;
+    if (n < 2)
+      return;
+    int[][] d = { {0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int s = 0, x = 0, y = 0;
+    while (true) {
+      if (check(m, n, d, s, x, y))
+        s++;
+      if (s == 4)
+        break;
+      x += d[s][0];
+      y += d[s][1];
+      search(board, m, n, d, x, y);
+    }
+    for (int i = 0; i < m; i++)
+      for (int j = 0; j < n; j++)
+        if (board[i][j] == '.')
+          board[i][j] = 'O';
+        else if (board[i][j] == 'O')
+          board[i][j] = 'X';
+  }
 
-		Stack<Pair> stack = new Stack<>();
-		for (int i = 0; i < x; i++)
-			for (int j = 0; j < y; j++)
-				if (board[i][j] == 'O'
-						&& (i == 0 || i == x - 1 || j == 0 || j == y - 1))
-				{
-					board[i][j] = '+';
-					if (i > 0)
-						stack.push(new Pair(i - 1, j));
-					if (i < x - 1)
-						stack.push(new Pair(i + 1, j));
-					if (j > 0)
-						stack.push(new Pair(i, j - 1));
-					if (j < y - 1)
-						stack.push(new Pair(i, j + 1));
-					while (!stack.isEmpty())
-					{
-						Pair pair = stack.pop();
-						if (board[pair.x][pair.y] != 'O')
-							continue;
-						board[pair.x][pair.y] = '+';
-						if (pair.x > 0)
-							stack.push(new Pair(pair.x - 1, pair.y));
-						if (pair.x < x - 1)
-							stack.push(new Pair(pair.x + 1, pair.y));
-						if (pair.y > 0)
-							stack.push(new Pair(pair.x, pair.y - 1));
-						if (pair.y < y - 1)
-							stack.push(new Pair(pair.x, pair.y + 1));
-					}
-				}
+  private boolean check(int m, int n, int[][] d, int s, int x, int y) {
+    x += d[s][0];
+    y += d[s][1];
+    return x < 0 || x >= m || y < 0 || y >= n;
+  }
 
-		for (int i = 0; i < x; i++)
-			for (int j = 0; j < y; j++)
-				if (board[i][j] == 'O')
-					board[i][j] = 'X';
-				else if (board[i][j] == '+')
-					board[i][j] = 'O';
-	}
-
-	public static void main(String[] args)
-	{
-		SurroundedRegions solution = new SurroundedRegions();
-		char[][] board = new char[3][3];
-		board[0][0] = 'X';
-		board[0][1] = 'X';
-		board[0][2] = 'X';
-		board[1][0] = 'X';
-		board[1][1] = 'O';
-		board[1][2] = 'X';
-		board[2][0] = 'O';
-		board[2][1] = 'X';
-		board[2][2] = 'X';
-		solution.solve(board);
-		System.out.println();
-	}
-
+  private void search(char[][] board, int m, int n, int[][] d, int x, int y) {
+    if (board[x][y] == 'O') {
+      Stack<Pair> stack = new Stack<>();
+      stack.push(new Pair(x, y));
+      while (!stack.isEmpty()) {
+        Pair p = stack.pop();
+        board[p.x][p.y] = '.';
+        for (int i = 0; i < 4; i++)
+          if (!check(m, n, d, i, p.x, p.y) && board[p.x + d[i][0]][p.y + d[i][1]] == 'O')
+            stack.push(new Pair(p.x + d[i][0], p.y + d[i][1]));
+      }
+    }
+  }
 }
