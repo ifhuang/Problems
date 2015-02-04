a6c41f4d4c31b9440ef8b2e00485358a3a320a06
Yifu Huang <ifhuang91@gmail.com>
2015-02-05 01:56:22 +0800

    update

--- src/leetcode/PalindromePartitioningII.java
+++ src/leetcode/PalindromePartitioningII.java
@@ -1,48 +1,20 @@
 package leetcode;
 
-public class PalindromePartitioningII
-{
-	public int minCut(String s)
-	{
-		if (s == null || s.length() < 2)
-			return 0;
-		else
-		{
-			int[] ans = new int[s.length()];
-			boolean[][] dp = new boolean[ans.length][ans.length];
-			ans[0] = 0;
-			dp[0][0] = true;
-			for (int i = 1; i < ans.length; i++)
-			{
-				ans[i] = i;
-				for (int j = 0; j <= i; j++)
-				{
-					if (j == i)
-						dp[j][i] = true;
-					else
-					{
-						if (s.charAt(j) == s.charAt(i))
-							if (j + 1 == i)
-								dp[j][i] = true;
-							else
-								dp[j][i] = dp[j + 1][i - 1];
-					}
-					if (dp[j][i])
-					{
-						if (j == 0)
-							ans[i] = 0;
-						else
-							ans[i] = Math.min(ans[i], ans[j - 1] + 1);
-					}
-				}
-			}
-			return ans[ans.length - 1];
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		PalindromePartitioningII solution = new PalindromePartitioningII();
-		System.out.println(solution.minCut("efe"));
-	}
+// https://oj.leetcode.com/problems/palindrome-partitioning-ii/
+public class PalindromePartitioningII {
+  public int minCut(String s) {
+    int n = s.length();
+    boolean[][] dp = new boolean[n][n];
+    int[] ans = new int[n];
+    for (int end = 0; end < n; end++) {
+      ans[end] = end;
+      for (int start = 0; start <= end; start++) {
+        if (s.charAt(start) == s.charAt(end))
+          dp[start][end] = (start == end || start + 1 == end ? true : dp[start + 1][end - 1]);
+        if (dp[start][end])
+          ans[end] = (start == 0 ? 0 : Math.min(ans[end], ans[start - 1] + 1));
+      }
+    }
+    return ans[n - 1];
+  }
 }
