01e9456817707b3066dc958a5d6771a280ec5485
Yifu Huang <ifhuang91@gmail.com>
2015-02-11 02:00:28 +0800

    update

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -1,25 +1,11 @@
 package leetcode;
 
-import java.util.HashSet;
-
-public class SingleNumber
-{
-	public int singleNumber(int[] A)
-	{
-		if (A == null || A.length == 0)
-			return 0;
-		HashSet<Integer> set = new HashSet<>();
-		for (int i = 0; i < A.length; i++)
-		{
-			if (set.contains(A[i]))
-			{
-				set.remove(A[i]);
-			}
-			else
-			{
-				set.add(A[i]);
-			}
-		}
-		return (int) set.toArray()[0];
-	}
+// https://oj.leetcode.com/problems/single-number/
+public class SingleNumber {
+  public int singleNumber(int[] A) {
+    int ans = 0;
+    for (int a : A)
+      ans ^= a;
+    return ans;
+  }
 }
--- src/leetcode/SingleNumberII.java
+++ src/leetcode/SingleNumberII.java
@@ -1,26 +1,14 @@
 package leetcode;
 
-import java.util.HashMap;
-
-public class SingleNumberII
-{
-	public int singleNumber(int[] A)
-	{
-		if (A == null || A.length == 0)
-			return 0;
-		HashMap<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < A.length; i++)
-		{
-			if (map.containsKey(A[i]))
-				map.put(A[i], map.get(A[i]) + 1);
-			else
-				map.put(A[i], 1);
-		}
-		for (int key : map.keySet())
-		{
-			if (map.get(key) != 3)
-				return key;
-		}
-		return 0;
-	}
+// https://oj.leetcode.com/problems/single-number-ii/
+public class SingleNumberII {
+  public int singleNumber(int[] A) {
+    int first = 0;
+    int second = 0;
+    for (int a : A) {
+      first = (first ^ a) & ~second;
+      second = (second ^ a) & ~first;
+    }
+    return first;
+  }
 }
