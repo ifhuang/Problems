25b60735c9c3bbeae26393b9d377c22f021b301f
Yifu Huang <ifhuang91@gmail.com>
2015-02-14 00:53:56 +0800

    update

--- src/leetcode/InsertionSortList.java
+++ src/leetcode/InsertionSortList.java
@@ -2,39 +2,30 @@ package leetcode;
 
 import leetcode.util.ListNode;
 
-public class InsertionSortList
-{
-	public ListNode insertionSortList(ListNode head)
-	{
-		if (head == null || head.next == null)
-			return head;
-		ListNode preHead = new ListNode(0);
-		preHead.next = head;
-		ListNode preMinNode = preHead;
-		ListNode minNode = head;
-		ListNode p1 = head;
-		ListNode p2 = head.next;
-		while (p2 != null)
-		{
-			if (p2.val < minNode.val)
-			{
-				preMinNode = p1;
-				minNode = p2;
-			}
-			p1 = p1.next;
-			p2 = p2.next;
-		}
-		if (minNode == head)
-		{
-			head.next = insertionSortList(head.next);
-			return head;
-		}
-		else
-		{
-			preMinNode.next = minNode.next;
-			minNode.next = head;
-			minNode.next = insertionSortList(minNode.next);
-			return minNode;
-		}
-	}
+// https://oj.leetcode.com/problems/insertion-sort-list/
+public class InsertionSortList {
+  public ListNode insertionSortList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+    ListNode min = head;
+    ListNode pre = null;
+    ListNode p = head;
+    ListNode q = head.next;
+    while (q != null) {
+      if (q.val < min.val) {
+        pre = p;
+        min = q;
+      }
+      p = p.next;
+      q = q.next;
+    }
+    if (pre == null) {
+      head.next = insertionSortList(head.next);
+      return head;
+    } else {
+      pre.next = min.next;
+      min.next = insertionSortList(head);
+      return min;
+    }
+  }
 }
--- src/leetcode/LRUCache.java
+++ src/leetcode/LRUCache.java
@@ -1,38 +1,78 @@
 package leetcode;
 
-public class LRUCache
-{
-	private java.util.LinkedHashMap<Integer, Integer> map;
-	int size;
+import java.util.HashMap;
+import java.util.Map;
 
-	public LRUCache(int capacity)
-	{
-		size = capacity;
-		map = new java.util.LinkedHashMap<Integer, Integer>(size, 0.75f, true)
-		{
-			private static final long serialVersionUID = 1L;
+// https://oj.leetcode.com/problems/lru-cache/
+public class LRUCache {
+  private int c;
+  private Map<Integer, DNode> map;
+  private DNode head;
+  private DNode tail;
 
-			@Override
-			protected boolean removeEldestEntry(
-					java.util.Map.Entry<Integer, Integer> eldest)
-			{
-				return size() > size;
-			}
+  public LRUCache(int capacity) {
+    c = capacity;
+    map = new HashMap<>(capacity);
+    head = new DNode(0, 0);
+    tail = new DNode(0, 0);
+    head.post = tail;
+    tail.pre = head;
+  }
 
-		};
-	}
+  public int get(int key) {
+    if (map.containsKey(key)) {
+      DNode i = map.get(key);
+      remove(i);
+      addFirst(i);
+      return i.v;
+    } else
+      return -1;
+  }
 
-	public int get(int key)
-	{
-		Integer value = map.get(key);
-		if (value == null)
-			return -1;
-		else
-			return value;
-	}
+  public void set(int key, int value) {
+    if (map.containsKey(key)) {
+      DNode i = map.get(key);
+      remove(i);
+      i.v = value;
+      addFirst(i);
+    } else {
+      if (map.size() == c)
+        map.remove(removeLast().k);
+      DNode i = new DNode(key, value);
+      map.put(key, i);
+      addFirst(i);
+    }
+  }
 
-	public void set(int key, int value)
-	{
-		map.put(key, value);
-	}
+  private void remove(DNode n) {
+    n.pre.post = n.post;
+    n.post.pre = n.pre;
+  }
+
+  private void addFirst(DNode n) {
+    n.pre = head;
+    n.post = head.post;
+    head.post = n;
+    n.post.pre = n;
+  }
+
+  private DNode removeLast() {
+    DNode old = tail.pre;
+    tail.pre = old.pre;
+    old.pre.post = tail;
+    return old;
+  }
+}
+
+
+class DNode {
+  int k;
+  int v;
+  DNode pre;
+  DNode post;
+
+  DNode(int k, int v) {
+    this.k = k;
+    this.v = v;
+  }
 }
