4ebe6e4a1d614bc9713212ec7807e3662dc4cf0f
Yifu Huang <ifhuang91@gmail.com>
2015-03-25 01:53:14 +0800

    cf 2

---
+++ src/codeforces/RankList.java
@@ -0,0 +1,154 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class RankList {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    int n = nextInt();
+    int k = nextInt();
+    List<TeamRank> list = new ArrayList<>();
+    Map<TeamRank, Integer> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      TeamRank tr = new TeamRank(nextInt(), nextInt());
+      list.add(tr);
+      if (map.containsKey(tr))
+        map.put(tr, map.get(tr) + 1);
+      else
+        map.put(tr, 1);
+    }
+    Collections.sort(list);
+    out.println(map.get(list.get(k - 1)));
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static double nextDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static double[] nextDoubleArray(int len, int start) throws IOException {
+    double[] a = new double[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextDouble();
+    return a;
+  }
+
+  static BigInteger nextBigInteger() throws IOException {
+    return new BigInteger(next());
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static String nextLine() throws IOException {
+    tok = new StringTokenizer("");
+    return in.readLine();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+}
+
+
+class TeamRank implements Comparable<TeamRank> {
+  int p;
+  int t;
+
+  TeamRank(int a, int b) {
+    p = a;
+    t = b;
+  }
+
+  @Override
+  public int compareTo(TeamRank o) {
+    return p == o.p ? t - o.t : o.p - p;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + p;
+    result = prime * result + t;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    TeamRank other = (TeamRank) obj;
+    if (p != other.p)
+      return false;
+    if (t != other.t)
+      return false;
+    return true;
+  }
+
+}
