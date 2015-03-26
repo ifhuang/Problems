ca1775d5b9de2340477ad1ae94dc210490c05e5b
Yifu Huang <ifhuang91@gmail.com>
2015-03-26 13:06:58 +0800

    cf 8

---
+++ src/codeforces/CAPSlOCK.java
@@ -0,0 +1,109 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class CAPSlOCK {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    String line = nextLine();
+    int n = line.length();
+    if (n == 1 && line.equals(line.toLowerCase()))
+      out.println(line.toUpperCase());
+    else if (n == 1 && line.equals(line.toUpperCase()))
+      out.println(line.toLowerCase());
+    else if (line.equals(line.toUpperCase()))
+      out.println(line.toLowerCase());
+    else {
+      String head = line.substring(0, 1);
+      String post = line.substring(1);
+      if (head.equals(head.toLowerCase()) && post.equals(post.toUpperCase()))
+        out.println(head.toUpperCase() + post.toLowerCase());
+      else
+        out.println(line);
+    }
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static double nextDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static double[] nextDoubleArray(int len, int start) throws IOException {
+    double[] a = new double[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextDouble();
+    return a;
+  }
+
+  static BigInteger nextBigInteger() throws IOException {
+    return new BigInteger(next());
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static String nextLine() throws IOException {
+    tok = new StringTokenizer("");
+    return in.readLine();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+}
---
+++ src/codeforces/DoubleCola.java
@@ -0,0 +1,111 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class DoubleCola {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    String[] s = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    int max = 40;
+    long[] a = new long[max];
+    for (int i = 1; i < max; i++)
+      a[i] = 5 * ((long) 1 << (i - 1));
+    long n = nextLong();
+    int i = 0;
+    for (; i < max; i++)
+      if (n <= a[i])
+        break;
+      else
+        n -= a[i];
+    if (n % (a[i] / 5) == 0) {
+      n /= a[i] / 5;
+      n--;
+    } else
+      n /= a[i] / 5;
+    out.println(s[(int) n]);
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static double nextDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static double[] nextDoubleArray(int len, int start) throws IOException {
+    double[] a = new double[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextDouble();
+    return a;
+  }
+
+  static BigInteger nextBigInteger() throws IOException {
+    return new BigInteger(next());
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static String nextLine() throws IOException {
+    tok = new StringTokenizer("");
+    return in.readLine();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+}
