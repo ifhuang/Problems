9ce6f53dd55d59b79285bb72e43dc24075dee8f3
Yifu Huang <ifhuang91@gmail.com>
2015-04-13 14:02:34 +0800

    update

---
+++ src/codeforces/Handshakes.java
@@ -0,0 +1,131 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Handshakes {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    int n = nextInt();
+    int[] count = new int[n];
+    @SuppressWarnings("unchecked")
+    Stack<Integer>[] stack = new Stack[n];
+    for (int i = 1; i <= n; i++) {
+      int j = nextInt();
+      if (count[j] == 0)
+        stack[j] = new Stack<Integer>();
+      count[j]++;
+      stack[j].push(i);
+    }
+    StringBuilder sb = new StringBuilder();
+    int c = 0;
+    for (int i = 0; i >= 0 && i < n;)
+      if (count[i] > 0) {
+        count[i]--;
+        sb.append(stack[i].pop() + " ");
+        i++;
+        c++;
+      } else
+        i -= 3;
+    if (c == n) {
+      out.println("Possible");
+      out.println(sb);
+    } else
+      out.println("Impossible");
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static double nextDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static double[] nextDoubleArray(int len, int start) throws IOException {
+    double[] a = new double[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextDouble();
+    return a;
+  }
+
+  static BigInteger nextBigInteger() throws IOException {
+    return new BigInteger(next());
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static String nextLine() throws IOException {
+    tok = new StringTokenizer("");
+    return in.readLine();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+  static boolean hasNext() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      String s = in.readLine();
+      if (s == null) {
+        return false;
+      }
+      tok = new StringTokenizer(s);
+    }
+    return true;
+  }
+
+}
---
+++ src/leetcode/algorithms/NumberofIslands.java
@@ -0,0 +1,34 @@
+package leetcode.algorithms;
+
+public class NumberofIslands {
+
+  private int count = 0;
+  private int n = 0;
+  private int m = 0;
+  private int[][] d = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+  public int numIslands(char[][] grid) {
+    n = grid.length;
+    if (n == 0)
+      return count;
+    m = grid[0].length;
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++)
+        if (grid[i][j] == '1') {
+          count++;
+          dfs(grid, i, j);
+        }
+    return count;
+  }
+
+  private void dfs(char[][] grid, int i, int j) {
+    grid[i][j] = '0';
+    for (int k = 0; k < d.length; k++) {
+      int x = i + d[k][0];
+      int y = j + d[k][1];
+      if (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == '1')
+        dfs(grid, x, y);
+    }
+  }
+
+}
