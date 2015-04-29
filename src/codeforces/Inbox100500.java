1b12c13ad4af24b06ab25c013515f078f5ce029e
Yifu Huang <ifhuang91@gmail.com>
2015-04-29 17:22:18 +0800

    cf 5

---
+++ src/codeforces/Inbox100500.java
@@ -0,0 +1,116 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class Inbox100500 {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    int n = nextInt();
+    int[] a = nextIntArray(n + 1, 1);
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+      if (a[i] == 1)
+        if (a[i - 1] == 1)
+          ans++;
+        else
+          ans += 2;
+    if (ans > 0)
+      ans--;
+    out.println(ans);
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static double nextDouble() throws IOException {
+    return Double.parseDouble(next());
+  }
+
+  static double[] nextDoubleArray(int len, int start) throws IOException {
+    double[] a = new double[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextDouble();
+    return a;
+  }
+
+  static BigInteger nextBigInteger() throws IOException {
+    return new BigInteger(next());
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static String nextLine() throws IOException {
+    tok = new StringTokenizer("");
+    return in.readLine();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+  static boolean hasNext() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      String s = in.readLine();
+      if (s == null) {
+        return false;
+      }
+      tok = new StringTokenizer(s);
+    }
+    return true;
+  }
+
+}
