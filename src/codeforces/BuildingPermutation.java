c0f4b60a031650c9417ef1fb7cb220638b380215
Yifu Huang <ifhuang91@gmail.com>
2015-05-03 22:59:24 +0800

    cf 3

---
+++ src/codeforces/BuildingPermutation.java
@@ -0,0 +1,81 @@
+package codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class BuildingPermutation {
+
+  static BufferedReader in;
+  static PrintWriter out;
+  static StringTokenizer tok;
+
+  static void solve() throws Exception {
+    int n = nextInt();
+    long[] a = nextLongArray(n, 0);
+    shuffleArray(a);
+    Arrays.sort(a);
+    long ans = 0;
+    for (int i = 0; i < n; i++)
+      ans += Math.abs(a[i] - (i + 1));
+    out.println(ans);
+  }
+
+  public static void main(String args[]) {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(new OutputStreamWriter(System.out));
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  static int nextInt() throws IOException {
+    return Integer.parseInt(next());
+  }
+
+  static int[] nextIntArray(int len, int start) throws IOException {
+    int[] a = new int[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextInt();
+    return a;
+  }
+
+  static long nextLong() throws IOException {
+    return Long.parseLong(next());
+  }
+
+  static long[] nextLongArray(int len, int start) throws IOException {
+    long[] a = new long[len];
+    for (int i = start; i < len; i++)
+      a[i] = nextLong();
+    return a;
+  }
+
+  static String next() throws IOException {
+    while (tok == null || !tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  static void shuffleArray(long[] array) {
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+      int index = random.nextInt(i + 1);
+      long temp = array[index];
+      array[index] = array[i];
+      array[i] = temp;
+    }
+  }
+
+}
