5c298937787a0704963bc56665b5cd9276f69409
Yifu Huang <ifhuang91@gmail.com>
2015-07-15 23:34:28 +0800

    lc

---
+++ src/leetcode/algorithms/DeleteNodeinaLinkedList.java
@@ -0,0 +1,16 @@
+package leetcode.algorithms;
+
+public class DeleteNodeinaLinkedList {
+  public void deleteNode(ListNode node) {
+    ListNode post = node.next;
+    while (true) {
+      node.val = post.val;
+      if (post.next == null) {
+        node.next = null;
+        break;
+      }
+      node = post;
+      post = post.next;
+    }
+  }
+}
