d2cc24d0b26c286a5f8ba197f45b3cfb9a85c504
Yifu Huang <ifhuang91@gmail.com>
2015-08-23 01:47:50 +0800

    lc update

--- src/leetcode/algorithms/LongestPalindromicSubstring.java
+++ src/leetcode/algorithms/LongestPalindromicSubstring.java
@@ -2,27 +2,61 @@ package leetcode.algorithms;
 
 // https://oj.leetcode.com/problems/longest-palindromic-substring/
 public class LongestPalindromicSubstring {
+  // time O(n^2), space O(n^2)
   public String longestPalindrome(String s) {
     int n = s.length();
+    char[] c = s.toCharArray();
     boolean[][] dp = new boolean[n][n];
-    int a = 0, b = 1;
-    for (int i = 0; i < n - 1; i++) {
+    int start = 0, end = 1;
+    for (int i = 0; i < n; i++)
       dp[i][i] = true;
-      dp[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
-      if (dp[i][i + 1]) {
-        a = i;
-        b = i + 1 + 1;
+    for (int i = 0; i < n - 1; i++) {
+      if (c[i] == c[i + 1]) {
+        dp[i][i + 1] = true;
+        start = i;
+        end = i + 2;
       }
     }
-    dp[n - 1][n - 1] = true;
-    for (int diff = 2; diff < n; diff++)
-      for (int st = 0; st + diff < n; st++) {
-        dp[st][st + diff] = dp[st + 1][st + diff - 1] && s.charAt(st) == s.charAt(st + diff);
-        if (dp[st][st + diff]) {
-          a = st;
-          b = st + diff + 1;
+    for (int diff = 2; diff < n; diff++) {
+      for (int i = 0; i + diff < n; i++) {
+        if (dp[i + 1][i + diff - 1] && c[i] == c[i + diff]) {
+          dp[i][i + diff] = true;
+          start = i;
+          end = i + diff + 1;
         }
       }
-    return s.substring(a, b);
+    }
+    return s.substring(start, end);
+  }
+
+  // time O(n^2), space O(n) ?
+  public String longestPalindrome2(String s) {
+    int n = s.length();
+    char[] c = s.toCharArray();
+    boolean[][] dp = new boolean[3][];
+    int start = 0, end = 1;
+    dp[0] = new boolean[n];
+    for (int i = 0; i < n; i++)
+      dp[0][i] = true;
+    dp[1] = new boolean[n - 1];
+    for (int i = 0; i < n - 1; i++) {
+      if (c[i] == c[i + 1]) {
+        dp[1][i] = true;
+        start = i;
+        end = i + 2;
+      }
+    }
+    for (int diff = 2; diff < n; diff++) {
+      dp[2] = new boolean[n - diff];
+      for (int i = 0; i + diff < n; i++) {
+        if (dp[diff % 2][i + 1] && c[i] == c[i + diff]) {
+          dp[2][i] = true;
+          start = i;
+          end = i + diff + 1;
+        }
+      }
+      dp[diff % 2] = dp[2];
+    }
+    return s.substring(start, end);
   }
 }
