1b357c54e4b35dd70e2863e724348a4d3ef8528c
Yifu Huang <ifhuang91@gmail.com>
2015-08-27 00:13:24 +0800

    lc update

--- src/leetcode/algorithms/RegularExpressionMatching.java
+++ src/leetcode/algorithms/RegularExpressionMatching.java
@@ -2,26 +2,28 @@ package leetcode.algorithms;
 
 // https://oj.leetcode.com/problems/regular-expression-matching/
 public class RegularExpressionMatching {
+  // time O(n*m), space O(n*m)
   public boolean isMatch(String s, String p) {
-    int sL = s.length();
-    int pL = p.length();
-    boolean[][] dp = new boolean[sL + 1][pL + 1];
+    int n = s.length() + 1;
+    int m = p.length() + 1;
+    char[] a = s.toCharArray();
+    char[] b = p.toCharArray();
+    boolean[][] dp = new boolean[n][m];
     dp[0][0] = true;
-    for (int i = 1; i < sL + 1; i++)
-      dp[i][0] = false;
-    for (int i = 2; i < pL + 1; i++)
-      if (p.charAt(i - 1) == '*')
+    for (int i = 2; i < m; i++)
+      if (b[i - 1] == '*')
         dp[0][i] = dp[0][i - 2];
-    for (int i = 1; i < sL + 1; i++)
-      for (int j = 1; j < pL + 1; j++)
-        if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')
+    for (int i = 1; i < n; i++)
+      for (int j = 1; j < m; j++) {
+        if (a[i - 1] == b[j - 1] || b[j - 1] == '.')
           dp[i][j] = dp[i - 1][j - 1];
-        else if (p.charAt(j - 1) == '*') {
-          if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')
-            dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+        else if (b[j - 1] == '*') {
+          if (b[j - 2] == a[i - 1] || b[j - 2] == '.')
+            dp[i][j] = dp[i][j - 2] || dp[i][j - 1] || dp[i - 1][j];
           else
             dp[i][j] = dp[i][j - 2];
         }
-    return dp[sL][pL];
+      }
+    return dp[n - 1][m - 1];
   }
 }
