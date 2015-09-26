99d80565b67f03683b6904d3b2e2fd16f0ee2ad0
Yifu Huang <ifhuang91@gmail.com>
2015-09-26 23:13:26 +0800

    add hihocoder template

---
+++ src/hihocoder/util/Template1.java
@@ -0,0 +1,21 @@
+package hihocoder.util;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Template1 {
+  static Scanner in;
+  static PrintWriter out;
+
+  static void solve() {
+
+  }
+
+  public static void main(String[] args) {
+    in = new Scanner(System.in);
+    out = new PrintWriter(System.out);
+    solve();
+    out.close();
+    in.close();
+  }
+}
