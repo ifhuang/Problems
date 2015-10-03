eaaa0322f0149bbf541d3b39ab13682b325b264a
Yifu Huang <ifhuang91@gmail.com>
2015-10-04 00:09:13 +0800

    update

--- src/leetcode/algorithms/CountandSay.java
+++ src/leetcode/algorithms/CountandSay.java
@@ -2,24 +2,50 @@ package leetcode.algorithms;
 
 // https://oj.leetcode.com/problems/count-and-say/
 public class CountandSay {
+  // recursive
   public String countAndSay(int n) {
-    if (n == 1)
+    if (n == 1) {
       return "1";
+    }
     String pre = countAndSay(n - 1);
-    int count = 1;
-    char now = pre.charAt(0);
     StringBuilder sb = new StringBuilder();
-    for (int i = 1; i < pre.length(); i++) {
-      char c = pre.charAt(i);
-      if (c == pre.charAt(i - 1))
+    char[] c = pre.toCharArray();
+    char p = c[0];
+    int count = 1;
+    for (int i = 1; i < c.length; i++) {
+      if (c[i] == p) {
         count++;
-      else {
-        sb.append(count).append(now);
+      } else {
+        sb.append(count).append(p);
+        p = c[i];
         count = 1;
-        now = c;
       }
     }
-    sb.append(count).append(now);
+    sb.append(count).append(p);
     return sb.toString();
+  }
+
+  // iterative
+  public String countAndSay2(int n) {
+    int i = 1;
+    String s = "1";
+    while (i++ < n) {
+      char[] c = s.toCharArray();
+      char p = c[0];
+      int count = 1;
+      StringBuilder sb = new StringBuilder();
+      for (int j = 1; j < c.length; j++) {
+        if (c[j] == p) {
+          count++;
+        } else {
+          sb.append(count).append(p);
+          p = c[j];
+          count = 1;
+        }
+      }
+      sb.append(count).append(p);
+      s = sb.toString();
+    }
+    return s;
   }
 }
--- src/leetcode/algorithms/SudokuSolver.java
+++ src/leetcode/algorithms/SudokuSolver.java
@@ -6,70 +6,107 @@ import java.util.Stack;
 
 // https://oj.leetcode.com/problems/sudoku-solver/
 public class SudokuSolver {
+  // backtracking
   public void solveSudoku(char[][] board) {
-    int n = 9;
+    int n = board.length;
+    int m = board[0].length;
     Stack<State> stack = new Stack<>();
-    char now = '0';
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < n; j++)
+    char pre = '0';
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
         if (board[i][j] == '.') {
-          now++;
-          board[i][j] = now;
-          while (!check(board, i, j)) {
-            now++;
-            if (now > '9')
+          char c = (char) (pre + 1);
+          for (; c <= '9'; c++) {
+            board[i][j] = c;
+            if (isValid(board, i, j)) {
+              stack.push(new State(i, j, c));
+              pre = '0';
               break;
-            board[i][j] = now;
-          }
-          if (now > '9') {
-            board[i][j] = '.';
-            State s = stack.pop();
-            board[s.x][s.y] = '.';
-            while (s.n == '9') {
-              s = stack.pop();
-              board[s.x][s.y] = '.';
             }
-            i = s.x;
-            j = s.y - 1;
-            now = s.n;
-          } else {
-            stack.push(new State(i, j, now));
-            now = '0';
+          }
+          if (c > '9') {
+            board[i][j] = '.';
+            State state = stack.pop();
+            i = state.x;
+            j = state.y;
+            board[i][j] = '.';
+            pre = state.c;
+            j--;
           }
         }
+      }
+    }
   }
 
-  private boolean check(char[][] board, int r, int c) {
-    int n = 9;
+  private boolean isValid(char[][] board, int x, int y) {
+    int n = board.length;
+    int m = board[0].length;
     Set<Character> set = new HashSet<>();
-    for (int j = 0; j < n; j++)
-      if (board[r][j] != '.')
-        if (!set.add(board[r][j]))
+    for (int i = 0; i < n; i++) {
+      if (board[i][y] != '.') {
+        if (!set.add(board[i][y])) {
           return false;
+        }
+      }
+    }
     set = new HashSet<>();
-    for (int j = 0; j < n; j++)
-      if (board[j][c] != '.')
-        if (!set.add(board[j][c]))
+    for (int j = 0; j < m; j++) {
+      if (board[x][j] != '.') {
+        if (!set.add(board[x][j])) {
           return false;
+        }
+      }
+    }
     set = new HashSet<>();
-    for (int i = r / 3 * 3; i < r / 3 * 3 + 3; i++)
-      for (int j = c / 3 * 3; j < c / 3 * 3 + 3; j++)
-        if (board[i][j] != '.')
-          if (!set.add(board[i][j]))
+    int a = x / 3;
+    int b = y / 3;
+    for (int i = a * 3; i < a * 3 + 3; i++) {
+      for (int j = b * 3; j < b * 3 + 3; j++) {
+        if (board[i][j] != '.') {
+          if (!set.add(board[i][j])) {
             return false;
+          }
+        }
+      }
+    }
     return true;
+  }
+
+  // recursion
+  public void solveSudoku2(char[][] board) {
+    dfs(board, 0, 0);
+  }
+
+  private boolean dfs(char[][] board, int x, int y) {
+    int n = board.length;
+    int m = board[0].length;
+    if (x == n) {
+      return true;
+    } else if (y == m) {
+      return dfs(board, x + 1, 0);
+    } else if (board[x][y] != '.') {
+      return dfs(board, x, y + 1);
+    } else {
+      for (char c = '1'; c <= '9'; c++) {
+        board[x][y] = c;
+        if (isValid(board, x, y) && dfs(board, x, y + 1)) {
+          return true;
+        }
+        board[x][y] = '.';
+      }
+      return false;
+    }
   }
 }
 
 
 class State {
-  public int x;
-  public int y;
-  public char n;
+  int x, y;
+  char c;
 
-  public State(int x, int y, char n) {
+  State(int x, int y, char c) {
     this.x = x;
     this.y = y;
-    this.n = n;
+    this.c = c;
   }
 }
